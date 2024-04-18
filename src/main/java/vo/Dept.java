3af6b333d2780680d6a0f148c86f5ed6be564f25
jjwwwp <wlghks789456@naver.com>
2024-04-18 10:16:53 +0900

    dept, emp테이블 VO생성

---
+++ src/main/java/vo/Dept.java
@@ -0,0 +1,9 @@
+package vo;
+
+// VO(Value Object) > DTO(DATA Transfer Object), Domain(테이블 속성값의 범위) 
+
+public class Dept {
+	public int deptNo;
+	public String dname;
+	public String loc;
+}
---
+++ src/main/java/vo/Emp.java
@@ -0,0 +1,12 @@
+package vo;
+
+public class Emp {
+	public int empno;
+	public String ename;
+	public String job;
+	public String mgr;
+	public String hiredate;
+	public double sal;
+	public double comm;
+	public int deptno;
+}
